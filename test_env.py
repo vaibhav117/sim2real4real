c15920984560c543b849be37eb7198a6df2dfd13
Karanbir Chahal <karanbleep@gmail.com>
2020-09-14 12:22:19 -0400

    test env

--- test_env.py
+++ test_env.py
@@ -1,8 +1,11 @@
 import gym
-
+import matplotlib.pyplot as plt
 env = gym.make('FetchReach-v1')
 
 while True:
     observation = env.reset()
+    img_obs = env.render(mode="rgb_array")
+    img_obs = env.render(mode="rgb_array", height=100, width=100)
+    plt.imshow(img_obs)
+    plt.show()
     goal = observation["desired_goal"]
-    print("hey")
