8f6a5ee1020b093012f6062c0a6ea3dd36ff8f0a
Karanbir Chahal <ksc487@cassio.cs.nyu.edu>
2020-09-22 00:59:10 -0400

    Let's see the plot

---
+++ run.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksc487/.mujoco/mujoco200/bin
+mpiexec --oversubscribe --use-hwthread-cpus -n 4 /misc/kcgscratch1/karan_exp/miniconda3/envs/rl/bin/python -u train.py --cuda --env-name='FetchPush-v1'| tee push.log 
--- sbatch.s
+++ sbatch.s
@@ -4,9 +4,11 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=10:00:00
-#SBATCH --gres=gpu:p40:1
-#SBATCH --mem=10GB
+#SBATCH --gres=gpu:v100:4
+#SBATCH --mem=30GB
 
 . ~/.bashrc
-cd /scratch/$USER/rl/sim2real4real/
-bash run_mujoco.sh
+#cd /scratch/$USER/rl/sim2real4real/
+module load mpi/openmpi-4.0
+cd /home/ksc487/sim2real4real
+bash run.sh
