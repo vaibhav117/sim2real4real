d0eca46ade090e4e25ad985dfe38098a751665fc
Karanbir Chahal <karanbleep@gmail.com>
2020-09-23 09:50:21 -0400

    ,

--- sbatch.s
+++ sbatch.s
@@ -2,9 +2,9 @@
 #
 #SBATCH --job-name=classification
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=10:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=4
+#SBATCH --time=20:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --mem=50GB
 
 . ~/.bashrc
@@ -12,3 +12,4 @@
 module load mpi/openmpi-4.0
 cd /home/ksc487/sim2real4real
 bash run.sh
+
