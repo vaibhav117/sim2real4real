302aae5fff062b1dcb9530aa99652009c06465f9
Karanbir Chahal <karanbleep@gmail.com>
2020-10-17 03:53:14 -0400

    sbatch script scae

--- sbatch.s
+++ sbatch.s
@@ -4,14 +4,14 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=30:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:v100:4
 #SBATCH --mem=50GB
 
 . ~/.bashrc
 #cd /scratch/$USER/rl/sim2real4real/
 module load mpi/openmpi-4.0
 source activate rl
-#pip install --upgrade --force-reinstall torch torchvision
+pip install --upgrade --force-reinstall torch torchvision
 cd /home/ksc487/sim2real4real
 bash run.sh
 
