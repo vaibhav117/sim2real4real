1ede922e1e76ccfe3981e9771545c8240569a72d
Karanbir Chahal <karanbleep@gmail.com>
2020-10-23 21:15:28 -0400

    I dunno

--- speed_test_mujoco.py
+++ speed_test_mujoco.py
@@ -6,16 +6,16 @@ env = gym.make('FetchReach-v1')
 env.reset()
 def benchmark_gpu_rendering(env):
     # add rendering context
-    #viewer = MjRenderContextOffscreen(env.env.sim, True, 2, "cuda")
-    #viewer.cam.distance = 1.2
-    #viewer.cam.azimuth = 180
-    #viewer.cam.elevation = -25
-    #env.env._viewers['rgb_array'] = viewer
+    viewer = MjRenderContextOffscreen(env.env.sim, True, 2, "cuda")
+    viewer.cam.distance = 1.2
+    viewer.cam.azimuth = 180
+    viewer.cam.elevation = -25
+    env.env._viewers['rgb_array'] = viewer
     avg_time = 0
     for i in range(100):
         s = time.time()
         env.step(env.action_space.sample())
-        im = env.sim.render(100,100)
+        im = env.render(mode='rgb_array')
         e = time.time()
         avg_time = avg_time + (e - s)
     print(avg_time / 100)
