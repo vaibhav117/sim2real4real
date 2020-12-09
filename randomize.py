2db35af119f41df1fb84e8e5e4e13146bd3f07d3
Karanbir Chahal <karanbleep@gmail.com>
2020-12-09 15:40:36 -0500

    commiting randomize

--- randomize.py
+++ randomize.py
@@ -10,11 +10,16 @@ import random
 import torch
 from mujoco_py.generated import const
 import matplotlib.pyplot as plt
+import cv2
+
+def show_video(img1):
+    cv2.imshow('current frame', cv2.resize(img1, (200,200)))
+    cv2.waitKey(0)
 
 def randomize_camera(viewer):
-    viewer.cam.distance = random.randrange(1,2)
-    viewer.cam.azimuth = random.randint(100,250)
-    viewer.cam.elevation = random.randint(-45,0)
+    viewer.cam.distance = random.randrange(1,3)
+    viewer.cam.azimuth = random.randint(160,190)
+    viewer.cam.elevation = random.randint(-45,-25)
 
 def randomize_textures(sim):
     for name in sim.model.geom_names:
@@ -39,12 +44,15 @@ env = gym.make('FetchPickAndPlace-v1')
 
 sim = env.sim
 viewer = MjRenderContextOffscreen(sim)
-viewer.cam.fixedcamid = 3
-viewer.cam.type = const.CAMERA_FIXED
+# viewer.cam.fixedcamid = 3
+viewer.cam.distance = 1.2
+viewer.cam.azimuth = 180
+viewer.cam.elevation = -25
+# viewer.cam.type = const.CAMERA_FIXED
 env.env._viewers['rgb_array'] = viewer
 im = env.render(mode="rgb_array")
-plt.imshow(im)
-plt.show()
+# plt.imshow(im)
+# plt.show()
 
 modder = TextureModder(sim)
 # modder = CameraModder(sim)
@@ -60,15 +68,20 @@ while True:
     
     randomize_textures(sim)
     # randomize_lights(sim)
-    if t % 10 == 0:
-        randomize_camera(viewer)
+    # if t % 10 == 0:
+    randomize_camera(viewer)
     # for name in sim.model.camera_names:
     #     loc = modder.get_pos(name) + torch.randn(3).numpy()
     #     # print(loc)
     #     modder.set_pos(name, loc)
-    #     # modder.set_fovy(name, t)
+    # #     # modder.set_fovy(name, t)
 
-    viewer.render()
+    # viewer.render()
+    im = env.render(mode="rgb_array")
+
+    plt.imshow(im)
+    plt.show()
+    # env.render()
     t += 1
     if t > 100 and os.getenv('TESTING') is not None:
         break
