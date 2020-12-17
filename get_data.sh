92aeae8c900341fbff65a034eaf1edd83324c3e6
Karanbir Chahal <karanbleep@gmail.com>
2020-12-17 02:20:01 -0500

    shit

--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,2 @@
-scp -r cassio:"/home/ksc487/sim2real4real/loss_plot.png /home/ksc487/sim2real4real/reward_plot.png" ./server_weights/
-#scp -r cassio:/home/ksc487/sim2real4real/saved_models/asym_goal_in_image/ ./server_weights/ 
+# scp -r cassio:"/home/ksc487/sim2real4real/loss_plot.png /home/ksc487/sim2real4real/reward_plot.png" ./server_weights/
+scp -r cassio:/home/ksc487/sim2real4real/saved_models/asym_goal_in_image/ ./server_weights/ 
