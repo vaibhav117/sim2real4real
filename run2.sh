8a2fc8e3e3644ffeb9bcff64be81c1ea95eff425
Karanbir Chahal <karanbleep@gmail.com>
2021-02-26 09:47:28 -0500

    Save changes

--- run2.sh
+++ run2.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksc487/.mujoco/mujoco200/bin
-mpiexec --oversubscribe --use-hwthread-cpus -n 4 /misc/kcgscratch1/karan_exp/miniconda3/envs/dmc/bin/python -u train.py --cuda --env-name='FetchSlide-v1' --mode='push' | tee fetch_pick_and_place.log 
+mpiexec --oversubscribe --use-hwthread-cpus -n 4 /misc/kcgscratch1/karan_exp/miniconda3/envs/dmc/bin/python -u train.py --cuda --env-name='FetchReach-v1' --mode='reach' | tee fetch_pick_and_place.log 
