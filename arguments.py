3f4465e9515aa834c47cc4e3aa3aca138cb2b4a7
Karanbir Chahal <karanbleep@gmail.com>
2021-04-03 15:20:46 -0400

    just save

--- arguments.py
+++ arguments.py
@@ -9,7 +9,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     # the environment setting
     parser.add_argument('--env-name', type=str, default='FetchPickAndPlace-v1', help='the environment name')
-    parser.add_argument('--n-epochs', type=int, default=500, help='the number of epochs to train the agent')
+    parser.add_argument('--n-epochs', type=int, default=1000, help='the number of epochs to train the agent')
     parser.add_argument('--n-cycles', type=int, default=10, help='the times to collect samples per epoch')
     parser.add_argument('--n-batches', type=int, default=10, help='the times to update the network')
     parser.add_argument('--save-interval', type=int, default=5, help='the interval that save the trajectory')
